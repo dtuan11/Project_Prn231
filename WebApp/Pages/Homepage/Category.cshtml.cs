a256d25210576eae6fefc6f113cc19cb19bcc340
dtuan11 <ntll0379@gmail.com>
2025-03-26 15:57:05 +0700

    fix bug homepage

--- WebApp/Pages/Homepage/Category.cshtml.cs
+++ WebApp/Pages/Homepage/Category.cshtml.cs
@@ -12,19 +12,29 @@ namespace WebApp.Pages.Homepage
     {
 
         private readonly PRN221_Project_1Context context;
+        private readonly HttpClient _httpClient;
 
-        public CategoryModel(PRN221_Project_1Context context)
+
+        public CategoryModel(PRN221_Project_1Context context, HttpClient httpClient)
         {
             this.context = context;
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7186"); // Replace with your API base URL
+
         }
 
         public List<API.Models.Book> Books { get; set; } = new List<API.Models.Book>();
-        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<CategoryResponse> Categories { get; set; } = new List<CategoryResponse>();
         public string? UserId { get; set; } = default!;
 
-        public IActionResult OnGet(int id)
+        public async Task<IActionResult> OnGetAsync(int id)
         {
-            Categories = context.Categories.ToList();
+            // Fetch categories
+            var categoriesResponse = await _httpClient.GetAsync("api/categories");
+            if (categoriesResponse.IsSuccessStatusCode)
+            {
+                Categories = await categoriesResponse.Content.ReadFromJsonAsync<List<CategoryResponse>>();
+            }
             UserId = HttpContext.Session.GetString("userId");
             Books = context.CategoryInBooks.Include(x => x.Book).Where(x => x.CateId == id && x.Book.Approve.Equals("Approved") && !x.Book.Status.Equals("Delete")).Select(x => x.Book).ToList();
             if (context.Categories.Find(id) != null)
